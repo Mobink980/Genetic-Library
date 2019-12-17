ea158c8084bd889d62558ca828b1fb2054db93c1
Ali Karazmoodeh <mobink980@gmail.com>
2019-12-18 00:00:24 +0330

    Add files via upload
    failure.php is a file shown when there was a problem uploading the file.

---
+++ failure.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<!--A php file to be shown when there is a problem for uploading the file.-->
+<html>
+<head>
+<meta charset="UTF-8">
+<title>upload</title>
+<!-- Bootstrap -->
+<link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="angular-1.7.8/angular.min.js"></script>
+<style>
+body{
+    background-image: url("park.jpg");
+}
+</style>
+
+</head>
+
+<body> <!--Showing a simple message to warn the user about the type of the file-->
+    <div class="container" style="background-color: black; color:white; text-align:center; margin-top:60px;">
+        <h3>Sorry! There was a problem uploading your file.</h3>
+    </div>
+
+<!-- Script -->
+<script>
+
+</script>
+</body>
+</html>
