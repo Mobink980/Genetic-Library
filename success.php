9538cdcb09eb4f79a56c2723c5ee7c2439d1219b
Ali Karazmoodeh <mobink980@gmail.com>
2019-12-18 00:02:18 +0330

    Add files via upload
    success.php is used when the upload of the file was successful.

---
+++ success.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<!--A php file to be shown when the upload was successful.-->
+<html>
+<head>
+<meta charset="UTF-8">
+<title>upload</title>
+<!-- Bootstrap -->
+<link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="angular-1.7.8/angular.min.js"></script>
+<style>
+body{
+    background-image: url("knight.jpg");
+}
+</style>
+
+</head>
+
+<body> <!--Showing a simple message to warn the user about the type of the file-->
+    <div class="container" style="background-color: black; color:white; text-align:center; margin-top:60px;">
+        <h3>Congratulations! Your upload was successful.</h3>
+    </div>
+
+<!-- Script -->
+<script>
+
+</script>
+</body>
+</html>
