d89a3e11b3cb399d7d9498228bb19d966ad59e63
MahdiSh <mahdishadroy@gmail.com>
2020-01-15 00:36:59 +0330

    comment system's files added

---
+++ comment/add_comment.php
@@ -0,0 +1,57 @@
+<?php
+
+/*  session_start();
+    $comment_name = $_SESSION["userUid"];
+    $comment_user_id = $_SESSION["userId"];
+    
+*/
+
+$connect = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+
+$error = '';
+$comment_name = '';
+$comment_content = '';
+
+if(empty($_POST["comment_name"]))
+{
+ $error .= '<p class="text-danger">Name is required</p>';
+}
+else
+{
+ $comment_name = $_POST["comment_name"];
+}
+
+if(empty($_POST["comment_content"]))
+{
+ $error .= '<p class="text-danger">Comment is required</p>';
+}
+else
+{
+ $comment_content = $_POST["comment_content"];
+}
+
+if($error == '')
+{
+ $query = "
+ INSERT INTO tbl_comment 
+ (parent_comment_id, comment, comment_sender_name) 
+ VALUES (:parent_comment_id, :comment, :comment_sender_name)
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':parent_comment_id' => $_POST["comment_id"],
+   ':comment'    => $comment_content,
+   ':comment_sender_name' => $comment_name
+  )
+ );
+ $error = '<label class="text-success">Comment Added</label>';
+}
+
+$data = array(
+ 'error'  => $error
+);
+
+echo json_encode($data);//send data into ajax
+
+?>
---
+++ comment/comment.php
@@ -0,0 +1,82 @@
+<?php
+//index.php
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Comment System using PHP and Ajax</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <br />
+  <br />
+  <div class="container">
+   
+   <form method="POST" id="comment_form">
+    <div class="form-group">
+     <input type="text" name="comment_name" id="comment_name" class="form-control" placeholder="Enter Name" />
+    </div>
+    <div class="form-group">
+     <textarea name="comment_content" id="comment_content" class="form-control" placeholder="Enter Comment" rows="5"></textarea>
+    </div>
+    <div class="form-group">
+     <input type="hidden" name="comment_id" id="comment_id" value="0" />
+     <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+    </div>
+   </form>
+   <span id="comment_message"></span>
+   <br />
+   <div id="display_comment"></div>
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+ 
+ $('#comment_form').on('submit', function(event){
+  event.preventDefault();
+  var form_data = $(this).serialize();//creat text string in standard URL
+  $.ajax({
+   url:"add_comment.php",
+   method:"POST",
+   data:form_data,
+   dataType:"JSON",
+   success:function(data)
+   {
+    if(data.error != '')
+    {
+     $('#comment_form')[0].reset(); //Syntax to convert jQuery element to a JavaScript object.because jQ dosen't have reset()
+     $('#comment_message').html(data.error);//sets the content (innerHTML) of the selected elements.
+     $('#comment_id').val('0');//The val() methodsets the value attribute of the selected elements.
+     load_comment();//show comment that user sends
+    }
+   }
+  })
+ });
+
+ load_comment();//show all comments
+
+ function load_comment()
+ {
+  $.ajax({
+   url:"fetch_comment.php",
+   method:"POST",
+   success:function(data)
+   {
+    $('#display_comment').html(data);
+   }
+  })
+ }
+
+ $(document).on('click', '.reply', function(){
+  var comment_id = $(this).attr("id");
+  $('#comment_id').val(comment_id);
+  $('#comment_name').focus(); //The focus event occurs when an element gets focus
+ });
+ 
+});
+</script>
